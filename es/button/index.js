9ab50097dbad6735a0cd73afc4969c0bc941577e
lvpangpang <574818075@qq.com>
2020-06-09 21:44:17 +0800

    丰富Icon

--- es/button/index.js
+++ es/button/index.js
@@ -2,7 +2,7 @@ import { createElement } from 'react';
 import TouchFeedback from 'rmc-feedback';
 import { s as styleInject } from '../style-inject.es-1f59c1d0.js';
 
-var css_248z = "/* 文字色 */\n/* 文字大小 */\n/* 主题色 */\n/* 圆角 */\n/* 水平间距 */\n/* 垂直间距 */\n.zec-button {\n  height: 48px;\n  line-height: 48px;\n  font-size: 16px;\n  color: #fff;\n}\n.zec-button-inner {\n  height: 100%;\n  border-radius: 5px;\n  text-align: center;\n  padding: 0 20px;\n}\n.zec-button-active {\n  color: #ddd !important;\n  opacity: 0.8;\n}\n.zec-button-primary {\n  background: #108ee9;\n}\n.zec-button-warning {\n  background: #ffc600;\n}\n.zec-button-success {\n  background: #6abf47;\n}\n.zec-button-error {\n  background: #f4333c;\n}\n.zec-button-disable {\n  opacity: 0.3;\n}\n";
+var css_248z = "/* 主题色 */\n/* 文字色 */\n/* 文字大小 */\n/* 圆角 */\n/* 图标大小 */\n/* 水平间距 */\n/* 垂直间距 */\n.zec-button {\n  height: 48px;\n  line-height: 48px;\n  font-size: 16px;\n  color: #fff;\n}\n.zec-button-inner {\n  height: 100%;\n  border-radius: 5px;\n  text-align: center;\n  padding: 0 20px;\n}\n.zec-button-active {\n  color: #ddd !important;\n  opacity: 0.8;\n}\n.zec-button-primary {\n  background: #108ee9;\n}\n.zec-button-warning {\n  background: #ffc600;\n}\n.zec-button-success {\n  background: #6abf47;\n}\n.zec-button-error {\n  background: #f4333c;\n}\n.zec-button-disable {\n  opacity: 0.3;\n}\n";
 styleInject(css_248z);
 
 function Button(props) {
