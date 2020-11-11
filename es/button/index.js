beaeeb50876dee1949bc48973f6bd0cf8677db05
lvpangpang <574818075@qq.com>
2020-11-11 17:18:14 +0800

    测试

--- es/button/index.js
+++ es/button/index.js
@@ -1,13 +1,28 @@
-import { createElement } from 'react';
+import {
+    createElement
+} from 'react';
 import TouchFeedback from 'rmc-feedback';
 import '../index-9d68b195.js';
 import '../style-inject.es-1f59c1d0.js';
-import { I as Icon } from '../index-39086b78.js';
+import {
+    I as Icon
+} from '../index-39086b78.js';
 import '../index-9e66fb76.js';
 
 function Button(props) {
-    var children = props.children, className = props.className, _a = props.disabled, disabled = _a === void 0 ? false : _a, _b = props.onClick, onClick = _b === void 0 ? function () { } : _b, _c = props.loading, loading = _c === void 0 ? false : _c, _d = props.type, type = _d === void 0 ? 'primary' : _d, inline = props.inline;
-    var _e = props.style, style = _e === void 0 ? {} : _e;
+    var children = props.children,
+        className = props.className,
+        _a = props.disabled,
+        disabled = _a === void 0 ? false : _a,
+        _b = props.onClick,
+        onClick = _b === void 0 ? function () {} : _b,
+        _c = props.loading,
+        loading = _c === void 0 ? false : _c,
+        _d = props.type,
+        type = _d === void 0 ? 'primary' : _d,
+        inline = props.inline;
+    var _e = props.style,
+        style = _e === void 0 ? {} : _e;
     var isClick = false;
     var _onClick = function () {
         if (!disabled) {
@@ -21,13 +36,28 @@ function Button(props) {
         }
     };
     if (inline) {
-        style = Object.assign({}, style, { 'display': 'inline-block' });
+        style = Object.assign({}, style, {
+            'display': 'inline-block'
+        });
     }
-    return (createElement("div", { style: style, className: "zec-button " + (className || ''), onClick: _onClick },
-        createElement(TouchFeedback, { activeClassName: !disabled ? 'zec-button-active' : '' },
-            createElement("div", { className: "zec-button-inner zec-button-" + type + " " + ((disabled || loading) ? 'zec-button-disable' : '') },
-                loading === true && createElement("span", { className: 'loading-right' },
-                    createElement(Icon, { type: 'loading', size: 'sm' })),
+    return (createElement("div", {
+            style: style,
+            className: "zec-button " + (className || ''),
+            onClick: _onClick
+        },
+        createElement(TouchFeedback, {
+                activeClassName: !disabled ? 'zec-button-active' : ''
+            },
+            createElement("div", {
+                    className: "zec-button-inner zec-button-" + type + " " + ((disabled || loading) ? 'zec-button-disable' : '')
+                },
+                loading === true && createElement("span", {
+                        className: 'loading-right'
+                    },
+                    createElement(Icon, {
+                        type: 'loading',
+                        size: 'sm'
+                    })),
                 children))));
 }
 
